a3e5fd66fdce37bd0f6dcfa98a25768f6a5d264b
HAYASHI Kentaro <kenhys@gmail.com>
2015-09-06 02:26:21 +0900

    Use propre genre code

--- update_genrelist.sh
+++ update_genrelist.sh
@@ -4,7 +4,12 @@
 
 if [ -f .bashrc ]; then
     . .bashrc
-    yomou genrerank $1 download
+    i=1
+    while [ $i -le 15 ]; do
+	yomou genrerank $1 download $i
+	i=`expr $i + 1`
+	sleep 3
+    done
     log $0 "execute genrerank $1 download"
 else
     log $0 "failed to execute genrerank $1 download"
